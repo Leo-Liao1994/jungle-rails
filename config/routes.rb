c5acfc9f3648f19bad0cd6243c45d49905f786d7
Your Name <akas3524@yahoo.com>
2020-11-12 21:56:53 +0000

    adding the About page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
-
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
+  resources :about, only: [:index]
 
   resource :cart, only: [:show] do
     post   :add_item
