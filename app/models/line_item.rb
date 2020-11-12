0ce72fe7d15a8549601ebea4e02685c5c9a07b6f
Your Name <akas3524@yahoo.com>
2020-11-12 02:19:25 +0000

    adding order details

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -6,4 +6,10 @@ class LineItem < ActiveRecord::Base
   monetize :item_price_cents, numericality: true
   monetize :total_price_cents, numericality: true
 
+  def get_product 
+    id = self.product_id
+    product = Product.find(id)
+    return product
+  end
+    
 end
