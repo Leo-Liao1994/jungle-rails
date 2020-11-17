cb549ffa0804ff03a95f639e3ba8d4cd6d34a5b6
Your Name <akas3524@yahoo.com>
2020-11-17 04:47:55 +0000

    user section

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,19 @@ Rails.application.routes.draw do
 
   end
 
+  # these routes are for showing users a login form, logging them in, and logging them out.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+    # These routes will be for signup. The first renders a form in the browse, the second will 
+    # receive the form and create a user in our database using the data given to us by the user.
+    get '/signup' => 'users#new'
+    post '/users' => 'users#create'
+
+end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -73,4 +86,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
+
